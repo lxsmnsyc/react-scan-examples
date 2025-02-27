fc0b2a54eed3452e9f1551231d03b75b790b3cc0
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-02-28 01:19:29 +0800

    Add vite template

---
+++ examples/vite/src/main.tsx
@@ -0,0 +1,13 @@
+import { scan } from 'react-scan';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+scan();
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
